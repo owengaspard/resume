5b4a3a24654f935cc89b500ff6477941ab5d8219
Owen Gaspard <owen@ogaspard.com>
2023-08-15 20:58:33 -0500

    dark mode update

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,14 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Owen Gaspard',
-  description: 'Owen Gaspard\'s resume',
+  description: 'Hey, I\'m Owen, a seventeen-year-old systems administrator and full-stack developer based in New Orleans.',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className='dark:bg-neutral-900'>{children}</body>
+      <body className='dark:bg-zinc-200'>{children}</body>
     </html>
   )
 }
